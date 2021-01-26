4d1c9e8747493626cbe73d84bfbcbc0a1bb494b1
Jackson Klopp <jklopp@paytrace.com>
2021-01-25 18:52:07 -0800

    loops algo

---
+++ WebFundamentals/JavaScript/loopsalgo.js
@@ -0,0 +1,14 @@
+for(var num = 1; num <=20; num++){
+    if (num % 2 == 1) {
+        console.log(num);
+    }
+}
+
+var num = 1; 
+var sum =  1;
+while (num <= 5) {        
+    console.log("num =" + num);    
+    console.log("sum =" +sum);
+    num=num+1;
+    sum=num + sum;
+}
