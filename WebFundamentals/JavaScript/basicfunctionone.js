8f417036593cc8bc4657a14425fde41a858b55f7
Jackson Klopp <jklopp@paytrace.com>
2021-01-31 09:40:34 -0800

    basic function 1

---
+++ WebFundamentals/JavaScript/basicfunctionone.js
@@ -0,0 +1,17 @@
+//total correct 11/15
+
+// 35
+//8
+//nothing - wrong
+//3,9
+//40
+// 4
+//10,3,30
+//3,4
+//nothing - wrong
+//0,1,2,3,4,5,6,7,8,9 x 2
+//nothing
+//nothing
+//10
+//nothing - wrong
+//15 -wrong it is 15, 15
