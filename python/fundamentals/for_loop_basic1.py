f3bb4785e7665537860b5141d3917d7b8c46478a
Jackson Klopp <jklopp@paytrace.com>
2021-02-10 18:42:13 -0800

    functions_basic2.py

--- python/fundamentals/for_loop_basic1.py
+++ python/fundamentals/for_loop_basic1.py
@@ -19,18 +19,18 @@
 #    else:
 #        print(x)
 
-#sum = 0
-#for x in range(1,500000,2):
-#    sum = sum + x
-#print(sum)
+sum = 0
+for x in range(1,500000,2):
+        sum = sum + x
+print(sum)
 
 #for x in range(2018, 0, -4):
 #    print(x)
 
-lownum = 0
-highnum = 10
-mult = 3
+# lownum = 0
+# highnum = 10
+# mult = 3
 
-for x in range(lownum, highnum,1):
-    if x % mult == 0:
-        print(x)
+# for x in range(lownum, highnum,1):
+#     if x % mult == 0:
+#         print(x)
