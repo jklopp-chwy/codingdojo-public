2defaa6a0a3946dd2fa48377b8433262260fce59
Jackson Klopp <jklopp@paytrace.com>
2021-02-15 16:30:51 -0800

    4th function done

--- python/fundamentals/functions_intermediate2.py
+++ python/fundamentals/functions_intermediate2.py
@@ -32,11 +32,25 @@ students = [
 #             print(key, ':', value)
 #iterateDictionary(students)
 
-def iterateDictionary2(key_name, some_list):
-    x = key_name
-    for i in range (len(some_list)):
-        name_dict = some_list[i]
-        for key, value in name_dict.items():
-            if key == x:
-                print(value)
-iterateDictionary2('first_name', students)
+# def iterateDictionary2(key_name, some_list):
+#     x = key_name
+#     for i in range (len(some_list)):
+#         name_dict = some_list[i]
+#         for key, value in name_dict.items():
+#             if key == x:
+#                 print(value)
+# iterateDictionary2('first_name', students)
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printInfo(some_dict):
+    for key in some_dict:
+            #print((len(some_dict[key])) + " " + key)
+            title = f"{len(some_dict[key])} {key}"
+            print(title)
+            for x in some_dict[key]:
+                print(x)
+printInfo(dojo)
