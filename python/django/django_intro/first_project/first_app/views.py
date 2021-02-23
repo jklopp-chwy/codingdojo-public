4b4377a96fa0f7f99e3e533e86a0f98d58b8e5d3
Jackson Klopp <jklopp@paytrace.com>
2021-02-22 18:12:50 -0800

    first project

--- python/django/django_intro/first_project/first_app/views.py
+++ python/django/django_intro/first_project/first_app/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 from django.shortcuts import render, HttpResponse
 def index(request):
-    return HttpResponse("This is my response!")
+    return HttpResponse("placeholder to later display a list of all blogs with a method named index")
+
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog with a method named new")
+
+def create(request):
+    return redirect('/')
+
+def number(request, number):
+    return HttpResponse(f"placeholder to display blog number: {number} with a method named show")
+
+def edit(request, number):
+    return HttpResponse(f"placeholder to edit blog {number} with a method named edit")
+
+def destroy(request, number):
+    return redirect('/')
