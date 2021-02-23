1ad480712330ff7033e1d6f1bfd7e84af9b4b5de
Jackson Klopp <jklopp@paytrace.com>
2021-02-22 17:23:44 -0800

    djano intro

---
+++ sql/queries/setup.sql
@@ -0,0 +1,11 @@
+select * from users;
+
+##insert into users (first_name,last_name,handle,birthday)
+##values ('jackson','klopp','jacksonklopp','1993-08-02');
+
+#update users
+#set first_name = 'jack'
+#where id = 6;
+
+#delete from users
+#where id = 6;
