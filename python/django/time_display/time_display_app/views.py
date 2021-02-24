81bb38b4ce14abcb464b7ca560f3004305c4182e
Jackson Klopp <jklopp@paytrace.com>
2021-02-23 17:56:02 -0800

    date_time

--- python/django/time_display/time_display_app/views.py
+++ python/django/time_display/time_display_app/views.py
@@ -3,6 +3,7 @@ from time import gmtime, strftime
     
 def index(request):
     context = {
-        "time": strftime("%Y-%m-%d %H:%M %p", gmtime())
+        #"time": strftime("%Y-%m-%d %H:%M %p", gmtime())
+        "time": strftime("%B %d, %Y %H:%M %p %Z", gmtime())
     }
     return render(request,'index.html', context)
