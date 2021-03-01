bb45e5f43edcfeb0e506913914adc1ff412f32f0
Jackson Klopp <jklopp@paytrace.com>
2021-02-28 19:36:49 -0800

    dojo survery revisted

--- python/django/dojo_survey/dojo_survey_app/views.py
+++ python/django/dojo_survey/dojo_survey_app/views.py
@@ -1,3 +1,10 @@
+#python manage.py migrate
+#python3 -m venv djangoPy3Env 
+# pip install Django==2.2 
+#django-admin startproject your_project_name_here
+#python manage.py startapp your_app_name_here
+#add new project to settings.py
+
 from django.shortcuts import render, redirect
 
 # Create your views here.
@@ -7,11 +14,11 @@ def index(request):
 def result(request):
     if request.method == 'GET':
         return redirect('/')
-    context = {
-        "fname": request.POST["fname"],
-        "lname": request.POST["lname"],
-        "dojos": request.POST["dojos"],
-        "gender": request.POST["gender"],
-        "food": request.POST.getlist("food[]")
-    }
-    return render(request, "result.html", context)
+    else:
+        request.session['fname'] = request.POST['fname']
+        request.session['lname'] = request.POST['lname']
+        request.session['dojos'] = request.POST['dojos']
+        request.session['gender'] = request.POST['gender']
+        request.session['food'] = request.POST.getlist("food[]")
+        request.session['counter'] = 100
+    return redirect('/')
