7cceae7fe596abf15df6cc229e3ce4bfd2aa498c
Jackson Klopp <jklopp@paytrace.com>
2021-03-02 20:59:23 -0800

    edits

--- python/django/ninja_gold/ninja_gold_app/views.py
+++ python/django/ninja_gold/ninja_gold_app/views.py
@@ -7,19 +7,36 @@
 
 from django.shortcuts import render, redirect
 from django.utils.crypto import get_random_string
+import random
+
+gold_values = {
+    "farm": (10,20),
+    "cave": (5,10),
+    "house": (2,5),
+    "casino": (0,50)
+}
 
 # Create your views here.
 def index(request):
-    request.session['gold'] = 0
-    return render(request, "index.html")
+    if not "gold" in request.session or "message" in request.session:
+        request.session['gold'] = 0
+        request.session['message'] = "welcome to ninja gold"
+    return render(request, 'index.html')
+
+def reset(request):
+    request.session.clear()
+    return redirect('/')
 
 def process_money(request):
+    message_log = request.POST['message']
+    gold_location = request.POST['location']
+    gold_range = gold_values[gold_location]
+    gold_amount = random.randint(gold_range[0],gold_range[1])
+
+
     if request.method == 'GET':
         return redirect('/')
     else:
-        request.session['gold'] += 1
-    return render(request,'process_money.html')
-
-def reset(request):
-    request.session.flush()
-    return redirect('/')
+        request.session['gold'] += gold_amount
+        request.session['message'] = message_log.append()
+    return render(request,'index.html')
