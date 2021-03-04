679703693b5a61bb44b08641b7cdf2db399453b5
Jackson Klopp <jklopp@paytrace.com>
2021-03-03 20:53:34 -0800

    user_templates

--- python/django/single_model_orm/users_app/views.py
+++ python/django/single_model_orm/users_app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils.crypto import get_random_string
+import random
+from .models import Users
 
 # Create your views here.
+def index(request):
+    context = {
+    	"all_the_users": Users.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def reset(request):
+    request.session.clear()
+    return redirect('/')
+
+def add_user(request):
+    if request.method == 'GET':
+        return redirect('/')
+    else:
+        new_user = Users.objects.create(
+            first_name=request.POST['first_name'], 
+            last_name =request.POST['last_name'],
+            email_address=request.POST['email_address'], 
+            age=request.POST['age'])
+    return redirect('/')
