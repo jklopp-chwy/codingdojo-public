d1a8b93e7bccf8f0bf46fa1a49b4d4178f059cf4
Jackson Klopp <jklopp@paytrace.com>
2021-03-14 14:27:42 -0700

    more routing

--- python/django/semi_restful/semi_restful_shows/urls.py
+++ python/django/semi_restful/semi_restful_shows/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.shows),
     path('shows', views.shows),
     path('shows/<int:id>', views.get_shows),
-    path('shows/<int:id>/delete', views.delete_shows)
+    path('shows/<int:id>/delete', views.delete_shows),
+    path('shows/new_show', views.new_shows),
+    path('shows/create_show', views.create_shows),
+    path('shows/<int:id>/edit_show', views.edit_shows),
+    path('shows/<int:id>/edit', views.edit)
 ]
--- python/django/semi_restful/semi_restful_shows/views.py
+++ python/django/semi_restful/semi_restful_shows/views.py
@@ -18,3 +18,34 @@ def delete_shows(request, id):
         show = Show.objects.get(id=id)
         show.delete()
     return redirect("/")
+
+def new_shows(request):
+    return render(request, 'new.html')
+
+def create_shows(request):
+    if request.method == 'GET':
+        return redirect('/')
+    else:
+        new_show = Show.objects.create(
+        title=request.POST['title'], 
+        description =request.POST['description'],
+        network=request.POST['network'], 
+        release_date=request.POST['release_date']
+        )
+    return redirect("/")
+
+def edit_shows(request, id):
+    context = {
+        "show": Show.objects.get(id=id)
+    }
+    return render(request, 'edit.html', context)
+
+def edit(request, id):
+    if request.method == "POST":
+        show = Show.objects.get(id=id)
+        show.title = request.POST['title'],
+        show.description = request.POST['description'],
+        show.network = request.POST['network'],
+        show.release_date = request.POST['release_date']
+        show.save()
+    return render(request, 'show.html')
