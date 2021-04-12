a00d1e2d3115bf2b544cf8b71d049510ba4d2182
Jackson Klopp <jklopp@paytrace.com>
2021-04-11 17:42:49 -0700

    todo1 strings

--- projectsandalgos/strings/todo1.py
+++ projectsandalgos/strings/todo1.py
@@ -1,2 +1,37 @@
 def removeBlanks(mystr):
-    return mystr
+    return "".join(mystr.split())
+string = 'p l a y that f un ky mus ic'
+print(removeBlanks(string))
+
+def getDigits(myStr):
+    return "".join([i for i in myStr if i.isdigit()])
+string1 = "005123125sdfzSDFGsdfgh21345235"
+print(getDigits(string1))
+
+def acronyms(myStr):
+    def cap(word):
+        cap = word[0].upper()+ " "
+        return cap
+    myList = myStr.split()
+    newWord = ""
+    for word in myList:
+        newWord = newWord + cap(word)
+    return newWord
+string2 = "the quick brown fox jumps over the lazy dog"
+print(acronyms(string2))
+
+def countNonSpaces(myStr):
+    count = len("".join(myStr.split()))
+    return count
+string3 = "abc abc"
+print(countNonSpaces(string3))
+
+def removeShort(arr, value):
+    tempArr = []
+    for word in arr:
+        length = len(word)
+        if length > value:
+            tempArr.append(word)
+    return tempArr
+array1 = ["my", "cat", "love", "pizza"]
+print(removeShort(array1, 4))
