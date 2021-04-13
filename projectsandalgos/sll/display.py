be678af5d67a4542065d148b5e3dada057704f40
Jackson Klopp <jklopp@paytrace.com>
2021-04-12 18:42:08 -0700

    length

---
+++ projectsandalgos/sll/display.py
@@ -0,0 +1,93 @@
+class Node(object):
+
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+
+##front
+    def addFront(self, data):
+        new_node = Node(data)
+        new_node.set_next(self.head)
+        self.head = new_node
+
+#remove front
+    def removeFront(self, data):
+        current = self.head
+        previous = None
+        found = False
+        while current and found is False:
+            if current.get_data() == data:
+                found = True
+            else:
+                previous = current
+                current = current.get_next()
+        if current is None:
+            raise ValueError("Data not in list")
+        if previous is None:
+            self.head = current.get_next()
+        else:
+            previous.set_next(current.get_next())
+#front
+    def front(self):
+        current = self.head
+        return current.data
+
+#contains
+    def search(self, data):
+        current = self.head
+        found = False
+        while current and found is False:
+            if current.get_data() == data:
+                found = True
+            else:
+                current = current.get_next()
+        if current is None:
+            print("Not found")
+        return current
+
+    def length(self):
+        current = self.head
+        count = 0
+        while current:
+            count += 1
+            current = current.get_next()
+        return count
+
+    def printNode(self):
+       curr = self.head
+       while curr:
+           print(curr.data)
+           curr = curr.get_next()
+
+
+myList = LinkedList()
+print("inserting")
+print(myList.addFront(5))
+print(myList.addFront(15))
+print(myList.addFront(25))
+print("Printing")
+myList.printNode()
+print("length")
+print(myList.length())
+print("deleting")
+print(myList.removeFront(5))
+print("Printing")
+myList.printNode()
+print("length")
+print(myList.length())
+print(myList.front())
+print("is the value below in the list?")
+print(myList.search(1))
